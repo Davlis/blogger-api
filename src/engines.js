e60fecc4afd655433e8a6895083b702a76a246e0
dliszka <dliszka@crodu.com>
2017-10-22 23:56:06 +0200

    Added engine dispatcher, created publishingEngine, taggingEngine folders

---
+++ src/engines.js
@@ -0,0 +1,39 @@
+const exec = require('child_process').exec;
+
+const publishingEngine = 
+    exec('node ./engines/publishingEngine/engine.js', (error, stdout, stderr) => {
+
+    console.log('stdout: ', stdout);
+    console.log('stderr: ', stderr);
+
+    if (error !== null) {
+        console.log('exec error: ', error);
+    }
+});
+
+publishingEngine.stdout.on('data', (data) => {
+    console.log(data);
+});
+
+publishingEngine.stderr.on('data', (data) => {
+    console.log(data);
+});
+
+const taggingEngine = 
+    exec('node ./engines/taggingEngine/engine.js', (error, stdout, stderr) => {
+
+    console.log('stdout: ', stdout);
+    console.log('stderr: ', stderr);
+
+    if (error !== null) {
+        console.log('exec error: ', error);
+    }
+});
+
+taggingEngine.stdout.on('data', (data) => {
+    console.log(data);
+});
+
+taggingEngine.stderr.on('data', (data) => {
+    console.log(data);
+});
