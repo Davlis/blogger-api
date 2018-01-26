ad6b1b6d9a58064f7d6cdc39d2b44569f74d0e08
davlis <ddewid@gmail.com>
2018-01-26 17:47:19 +0100

    Implementation of tfidf algorithm in taggingEngine.

--- src/engines/taggingEngine/engine.js
+++ src/engines/taggingEngine/engine.js
@@ -1,18 +1,84 @@
 import generateConfig from '../../config'
 import initSequelizeFromConfig from '../../database'
 
-const engineTimeInterval = process.env.TAGGING_INTERVAL || 24*60*60*1000
-
-const config = generateConfig('../../../.env')
-const { sequelize, models } = initSequelizeFromConfig(config)
+const engineTimeInterval = process.env.TAGGING_INTERVAL || 60*60*24
 
 process.on('unhandledRejection', console.error)
 
 handler().catch(err => console.log(err))
 
 async function handler() {
-    setInterval(start, engineTimeInterval)
+
+    const config = generateConfig('../../../.env')
+
+    const { sequelize, models } = initSequelizeFromConfig(config)
+
+    startInterval(engineTimeInterval, () => start(sequelize, models))
 }
 
-async function start() {
+async function start(sequelize, models) {
+    const { Blog, Post } = models
+
+    const blogs = await Blog.findAll({})
+
+    for (const blog of blogs) {
+        const posts = await Post.findAll({
+            where: {
+                blogId: blog.id,
+            }
+        })
+
+        for (const post of posts) {
+            const words = getWords(post.content)
+            const tfidf = {}
+            for (const word of words) {
+                if (!tfidf[word]) {
+                    tfidf[word] = getTf(word, words)*getIdf(word, posts)
+
+                    console.log('tf(', word, ') = ', getTf(word, words))
+                    console.log('idf(', word, ') = ', getIdf(word, posts))
+                    console.log('tfidf(', word, ') = ', tfidf[word])
+                }
+            }
+            console.log(tfidf)
+        }
+    }
+}
+
+function startInterval(seconds, callback) {
+    callback();
+    return setInterval(callback, seconds*1000);
+}
+
+function getWords(content) {
+    return content.split(/[ ,.;:?!@#$%^&*()]+/).filter(Boolean)
+}
+
+function getTf(word, words) {
+    return count(words, word)/words.length
+}
+
+function getIdf(word, documents) {
+    const documentLength = documents.length
+    let i = 0
+
+    for (const document of documents) {
+        const words = getWords(document.content)
+
+        if (words.includes(word)) {
+            ++i
+        }
+    }
+
+    return Math.log(documentLength/i)
+}
+
+function count(array, word) {
+    let i = 0
+    for (const item of array) {
+        if (item === word) {
+            ++i
+        }
+    }
+    return i
 }
