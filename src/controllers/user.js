290af1d5066ba41f29632be4ee439518355d6f78
davlis <ddewid@gmail.com>
2018-01-26 22:30:08 +0100

    Added updateUser. Added deleteUser.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,9 +2,32 @@ import { assertOrThrow, pick } from '../utils'
 import { USER_ROLES } from '../models/user'
 
 export async function updateUser(req, res) {
-    res.json({ status: 'Not implemented' })
+
+    const config = res.app.get('config')
+    const { User } = req.app.get('models')
+    let { user } = res.locals
+    const { email, password, lastName, firstName } = req.body
+
+    const passhash = User.hashPassword(password, config.salt)
+
+    user = await user.update({
+        email,
+        password,
+        lastName,
+        firstName,
+        passhash,
+        role: USER_ROLES.CUSTOMER,
+    })
+
+    res.json(user)
 }
 
 export async function deleteUser(req, res) {
-    res.json({ status: 'Not implemented' })
+
+    const { User } = req.app.get('models')
+    const { user } = res.locals
+
+    await user.destroy()
+
+    res.json({ status: 'ok' })
 }
