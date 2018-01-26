555caa88469b47ddf8c0bed6ee0915ea0d8560fd
davlis <ddewid@gmail.com>
2018-01-26 13:43:42 +0100

    Added user controller.

---
+++ src/controllers/user.js
@@ -0,0 +1,9 @@
+import { assertOrThrow } from '../utils'
+
+export async function updateUser(req, res) {
+    res.json({ status: 'Not implemented' })
+}
+
+export async function deleteUser(req, res) {
+    res.json({ status: 'Not implemented' })
+}
