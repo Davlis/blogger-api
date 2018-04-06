e7151a984c02c37d9c734b94676a3c9ee9eff98c
davlis <ddewid@gmail.com>
2018-04-06 22:28:16 +0200

    added badRequest class

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,11 @@
+export class badRequest extends Error {
+    constructor(message) {
+        super();
+        this.status = 400;
+        this.message = message;
+    }
+}
+
 export class notFound extends Error {
     constructor(message) {
         super();
